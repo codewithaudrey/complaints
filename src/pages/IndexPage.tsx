366787804e44a7e39bc6d7af6c7ccad2921a7523
codewithaudrey <fantasticaudrey001@gmail.com>
2024-07-04 12:59:07 +0100

    added new page component

---
+++ src/pages/IndexPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const IndexPage = () => {
+  return (
+    <div style={{maxWidth: '30rem'}}>
+        <p style={{fontSize: '1.4rem', color: '#0f0f0f', lineHeight: '30px', letterSpacing: '-0.24px', textAlign: 'center'}}>
+        Powerful user authentication that integrates in minutes. Free up to 10,500 MAU.
+        </p>
+    </div>
+  )
+}
+
+export default IndexPage
