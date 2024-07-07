835db4bcc1ceec4f8cb6136a5786fe769ac78aa1
codewithaudrey <fantasticaudrey001@gmail.com>
2024-07-07 16:31:03 +0100

    TextField component

---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  PolymorphicComponentWithRef,
+  PolymorphicRef,
+} from '../../types/polymorphic.types';
+import { VariantProps } from '../../types/cv.types';
+import textFieldVariants from './variants';
+import useField from '../../hooks/useField';
+
+const elements = ['input', 'textarea'] as const;
+
+namespace TextField {
+  export type Ref = PolymorphicRef<(typeof elements)[number]>;
+  export type Props = PolymorphicComponentWithRef<
+    (typeof elements)[number],
+    VariantProps<typeof textFieldVariants>
+  >;
+}
+
+const TextField = React.forwardRef(
+  (props: TextField.Props, ref: TextField.Ref) => {
+    const {
+      as,
+      style,
+      className,
+      onChange,
+      p,
+      px,
+      py,
+      ps,
+      pe,
+      pt,
+      pb,
+      border,
+      borderTop,
+      borderBottom,
+      borderLeft,
+      borderRight,
+      color,
+      borderColor,
+      borderStyle,
+      width,
+      height,
+      size,
+      align,
+      weight,
+      pseudos = { placeholder: { color: 'gray-70' } },
+      ...rest
+    } = props;
+    const Component = (as || 'input') as React.ElementType;
+    const { meta, helper } = useField();
+
+    const handleChange = (e: any) => {
+      helper.setValue(e.target.value);
+      onChange && onChange(e);
+    };
+
+    const handleBlur = () => helper.setTouched(false);
+    const handleFocus = () => helper.setTouched(true);
+
+    const defaultStyle: React.CSSProperties = {
+      outline: 'none',
+    };
+
+    return (
+      <Component
+        ref={ref}
+        className={textFieldVariants({
+          p,
+          px,
+          py,
+          ps,
+          pe,
+          pt,
+          pb,
+          border,
+          borderTop,
+          borderBottom,
+          borderLeft,
+          borderRight,
+          color,
+          borderColor,
+          borderStyle,
+          width,
+          height,
+          size,
+          align,
+          weight,
+          className,
+          pseudos,
+        })}
+        value={meta.value}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+        onChange={handleChange}
+        style={{ ...defaultStyle, ...style }}
+        {...rest}
+      />
+    );
+  }
+);
+
+export default TextField;
---
+++ src/components/TextField/variants.ts
@@ -0,0 +1,41 @@
+import border from '../../props/border';
+import color from '../../props/color';
+import sizing from '../../props/sizing';
+import spacing from '../../props/spacing';
+import typography from '../../props/typography';
+import cv from '../../utils/cv';
+
+const textFieldVariants = cv({
+  variants: {
+    p: spacing.p,
+    px: spacing.px,
+    py: spacing.py,
+    ps: spacing.ps,
+    pe: spacing.pe,
+    pt: spacing.pt,
+    pb: spacing.pb,
+    size: typography.size,
+    align: typography.align,
+    weight: typography.weight,
+    border: border.width,
+    borderTop: border.top,
+    borderBottom: border.bottom,
+    borderLeft: border.left,
+    borderRight: border.right,
+    borderStyle: border.style,
+    color: color.text,
+    borderColor: color.border,
+    backgroundColor: color.background,
+    width: sizing.width,
+    height: sizing.height,
+  },
+  defaultVariants: {
+    px: 'sm',
+    size: 16,
+    weight: 400,
+    height: 'full',
+    width: 'full',
+  },
+});
+
+export default textFieldVariants;
