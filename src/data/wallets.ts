02543f18827e16051e885399f5bcae9741ee2544
codewithaudrey <fantasticaudrey001@gmail.com>
2024-07-07 14:23:58 +0100

    wallets data

---
+++ src/data/wallets.ts
@@ -0,0 +1,123 @@
+const wallets = [
+  {
+    title: 'trust',
+    image: {
+      url: '/assets/wallets/trust.png',
+      alt: '',
+    },
+  },
+  {
+    title: '1inch',
+    image: {
+      url: '/assets/wallets/1inch.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'aave',
+    image: {
+      url: '/assets/wallets/aave.png',
+      alt: '',
+    },
+  },
+  {
+    title: 'argent',
+    image: {
+      url: '/assets/wallets/argent.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'binance',
+    image: {
+      url: '/assets/wallets/binance.png',
+      alt: '',
+    },
+  },
+  {
+    title: 'bitpay',
+    image: {
+      url: '/assets/wallets/bitpay.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'coinbase',
+    image: {
+      url: '/assets/wallets/coinbase.png',
+      alt: '',
+    },
+  },
+  {
+    title: 'defi',
+    image: {
+      url: '/assets/wallets/defi.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'flare',
+    image: {
+      url: '/assets/wallets/flare.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'halo',
+    image: {
+      url: '/assets/wallets/halo.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'linen',
+    image: {
+      url: '/assets/wallets/linen.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'metamask',
+    image: {
+      url: '/assets/wallets/metamask.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'phantom',
+    image: {
+      url: '/assets/wallets/phantom.png',
+      alt: '',
+    },
+  },
+  {
+    title: 'pillar',
+    image: {
+      url: '/assets/wallets/pillar.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'torus',
+    image: {
+      url: '/assets/wallets/torus.jpg',
+      alt: '',
+    },
+  },
+  {
+    title: 'tron',
+    image: {
+      url: '/assets/wallets/tron.png',
+      alt: '',
+    },
+  },
+  {
+    title: 'wallet IO',
+    image: {
+      url: '/assets/wallets/walletio.jpg',
+      alt: '',
+    },
+  },
+];
+
+export default wallets;
